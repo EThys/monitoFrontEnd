96eec849d439b3ee2d10e4d902d524b3e4047401
EThys <ethbergmuzola500@gmail.com>
2025-05-16 14:08:03 +0100

    Feat:Mise en place de la page d'accueil du site

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<!-- eslint-disable @typescript-eslint/ban-ts-comment -->
+<script lang="ts" setup>
+//@ts-ignore
+import HeroComponent from '@/components/hero/HeroComponent.vue';
+//@ts-ignore
+import FooterComponent from '@/components/footer/FooterComponent.vue';
+//@ts-ignore
+import ServiceComponent from '@/components/services/ServiceComponent.vue';
 
+
+</script>
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <HeroComponent/>
+    <ServiceComponent/>
+    <FooterComponent/>
+
 </template>
+<style>
+</style>
